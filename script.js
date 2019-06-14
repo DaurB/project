d3713f6eb0923dfb6f8084bb149a555e00f2e05f
Dauren <biboldauren@gmail.com>
2019-06-14 19:05:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var number = 5;
+var symbol = "Hello";
+console.log(symbol);
+
+var persone = {
+    name: "Dauren",
+    age: 20,
+    weight: 53
+}
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = [1, 2, 3, 4, 5];
+//console.log(arr[0]);
+//console.log(arr[2]);
+
+//alert("Hello World");
+
+// let a = +prompt("Ты тупой?", "Yes");
+//console.log(a);
+
+// console.log(typeof(a));
+
+// console.log("Dauren" + " - sexy");
+// console.log(854 + +" - sexy");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(--incr);
+console.log(++decr);
+
+console.log(5%1);
+
+console.log("5" === 5);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+var a = 5;
+console.log(a);
+
